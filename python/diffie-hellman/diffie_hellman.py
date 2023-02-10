a3ad7799f0974daeab946ca2df7d535e2be987e4
Cédric Skwar <cdrc@skwar.me>
2023-02-10 21:41:01 +0100

    Added a implementation of the diffie hellman key excahnge

---
+++ python/diffie-hellman/diffie_hellman.py
@@ -0,0 +1,10 @@
+import random
+
+def private_key(p):
+    return random.randint(1, p - 1)
+
+def public_key(p, g, private):
+    return pow(g, private, p)
+
+def secret(p, public, private):
+    return pow(public, private, p)
