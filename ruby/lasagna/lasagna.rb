fc89147137b7a602982443d801caf53e5d68112b
Cédric Skwar <cdrc@skwar.me>
2023-02-14 23:11:50 +0100

    Added lasagna for ruby.

---
+++ ruby/lasagna/lasagna.rb
@@ -0,0 +1,16 @@
+class Lasagna
+  EXPECTED_MINUTES_IN_OVEN = 40
+  def remaining_minutes_in_oven(actual_minutes_in_oven)
+    EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
+  end
+
+  def preparation_time_in_minutes(layers)
+    layer_prep_time = 2
+    layer_prep_time * layers
+  end
+
+  def total_time_in_minutes(number_of_layers:, actual_minutes_in_oven:)
+    time_preping = preparation_time_in_minutes(number_of_layers)
+    time_preping + actual_minutes_in_oven
+  end
+end
