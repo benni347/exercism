0b9a6d03b354ef99fc286552ae0b60e2b7dfacdf
Cedric Skwar <cdrc@5y5.one>
2023-11-25 13:39:47 +0100

    roman numeralS

---
+++ rust/roman-numerals/src/lib.rs
@@ -0,0 +1,39 @@
+use std::fmt::{Display, Formatter, Result};
+
+pub struct Roman(String);
+
+impl Display for Roman {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl From<u32> for Roman {
+    fn from(mut num: u32) -> Self {
+        let mut result = String::new();
+        let numerals = [
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I"),
+        ];
+
+        for &(value, symbol) in numerals.iter() {
+            while num >= value {
+                num -= value;
+                result.push_str(symbol);
+            }
+        }
+
+        Roman(result)
+    }
+}
